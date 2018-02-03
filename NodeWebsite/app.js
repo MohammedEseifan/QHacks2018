3608c4474152e458c8a72eea2a3bc97ea6767f56
Mohammed Eseifan <m.eseifan@gmail.com>
2018-02-03 03:28:15 -0500

    Added node app for website

---
+++ NodeWebsite/app.js
@@ -0,0 +1,43 @@
+var http = require('http');
+var express = require('express');
+var api = require('instagram-node').instagram();
+var app = express();
+ 
+// app.configure(function() {
+//   // The usual... 
+// });
+ 
+api.use({
+  client_id: '0a8f5968db0141b985d4aa9574df3fe1',
+  client_secret: 'bb11d5ff4fda4b2390e9bdd286b63439 ' 
+});
+ 
+var redirect_uri = 'http://watchai.net/handleauth?test=1324';
+ 
+exports.authorize_user = function(req, res) {
+  res.redirect(api.get_authorization_url(redirect_uri));
+};
+ 
+exports.handleauth = function(req, res) {
+  api.authorize_user(req.query.code, redirect_uri, function(err, result) {
+    if (err) {
+      console.log(err.body);
+      res.send(err.body);
+    } else {
+      console.log('Yay! Access token is ' + result.access_token);
+      res.send('You made it!!');
+    }
+  });
+
+  req.query.instaName
+};
+ 
+// This is where you would initially send users to authorize 
+app.get('/authorize_user', exports.authorize_user);
+// This is your redirect URI 
+app.get('/handleauth', exports.handleauth);
+ 
+http.createServer(app).listen(3000, function(){
+  console.log("Express server listening on port " + 3000);
+});
+ 
--- stdlib-workspace/TheOnlyMohammed/mediaFilter/functions/__main__.js
+++ stdlib-workspace/TheOnlyMohammed/mediaFilter/functions/__main__.js
@@ -1,10 +1,25 @@
 /**
 * A basic Hello World function
-* @param {string} name Who you're saying hello to
+* @param {string} imageURL Who you're saying hello to
 * @returns {string}
 */
-module.exports = (name = 'world', context, callback) => {
 
-  callback(null, `hello ${name}`);
+const Clarifai = require('clarifai');
+
+
+module.exports = (imageURL = 'https://samples.clarifai.com/metro-north.jpg', context, callback) => {
+
+const clarifai = new Clarifai.App({
+  apiKey: process.env.CLARIFAI_API_KEY
+});
+	// Prediction on general model using video API
+	clarifai.models.predict(Clarifai.MODERATION_MODEL, imageURL).then(
+	  function(response) {
+	    callback(null, JSON.stringify(response));
+	  },
+	  function(err) {
+	     callback(null, JSON.stringify(err));
+	  });
+  
 
 };
