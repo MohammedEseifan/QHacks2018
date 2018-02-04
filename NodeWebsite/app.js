6bfb9ba267dbafa97dbfb5ae3cd6d12d5ce2add0
Mohammed Eseifan <m.eseifan@gmail.com>
2018-02-03 19:25:00 -0500

    Finalised ParseHub integration

--- NodeWebsite/app.js
+++ NodeWebsite/app.js
@@ -4,6 +4,9 @@ var ejs = require('ejs');
 var lib = require('lib');
 var api = require('instagram-node').instagram();
 var app = express();
+var request = require('request');
+
+
 
 app.set('view engine', 'ejs')
 // app.configure(function() {
@@ -17,6 +20,14 @@ api.use({
  
 var redirect_uri = 'http://localhost:3000/handleauth';
  var newRedirect_uri;
+
+
+function parseFollowers(data){
+  console.log(data)
+}
+
+
+
 exports.authorize_user = function (req, res) {
   newRedirect_uri = redirect_uri + '?username=' + req.query.username;
   res.redirect(api.get_authorization_url(redirect_uri+'?username='+req.query.username));
@@ -24,35 +35,46 @@ exports.authorize_user = function (req, res) {
  
 exports.handleauth = function(req, res) {
   
-  console.log(req.query.code);
-  console.log(newRedirect_uri);
+
   api.authorize_user(req.query.code, newRedirect_uri, function (err, result) {
-    console.log(result);
-    console.log(err);
+   
     if (err) {
       console.log(err.body);
-      // res.send(err.body);
     } else {
       console.log('Yay! Access token is ' + result.access_token);
-      // res.send('You made it!!');
     }
 
+    var username = req.query.username;
     api.use({ access_token: result.access_token });
 
-    console.log(JSON.stringify({userID:req.query.username, token:result.access_token }))
-    lib.TheOnlyMohammed.mediaFilter['@dev']({userID:req.query.username, token:result.access_token }, (err, result) => {
-      console.log(err);
-      console.log(JSON.stringify(result));
-      res.send(JSON.stringify(result));
-      // res.send(result);
+    request({
+      uri: 'https://www.parsehub.com/api/v2/projects/tETfMCbfN8Md/run',
+      method: 'POST',
+      form: {
+        api_key: "tr0EdoMBubaDWcHYw0C7taFd",
+        start_url: "https://www.instagram.com",
+        start_template: "main_template",
+        start_value_override: JSON.stringify({user: username}),
+        send_email: "0"
+      }
+    }, function (err, resp, body) {
+      console.log("running");
+      console.log(body);
+      var runID = JSON.parse(body).run_token;
+      setTimeout(checkRun, 20000, runID);
+      
     });
+    // lib.TheOnlyMohammed.mediaFilter['@dev']({userID:req.query.username, api:api }, (err, result) => {
+    //   console.log(err);
+    //   console.log(result);
+    // });
   });
 
-  // ejs.renderFile('views/results.ejs', { username: req.query.username }, null, function (err, str) {
-  //   console.log(err);
-  //   console.log(str);
-  //   res.send(str);
-  // });
+  ejs.renderFile('views/results.ejs', { username: req.query.username }, null, function (err, str) {
+    console.log(err);
+    console.log(str);
+    res.send(str);
+  });
 };
 
 exports.index = function(req,res){
@@ -70,3 +92,39 @@ http.createServer(app).listen(3000, function(){
   console.log("Express server listening on port " + 3000);
 });
  
+function checkRun(runID){
+  console.log("checking");
+  console.log(runID);
+  request({
+    uri: 'https://www.parsehub.com/api/v2/runs/'+runID,
+    method: 'GET',
+    qs: {
+      api_key: "tr0EdoMBubaDWcHYw0C7taFd"
+    }
+  }, function (err, resp, body) {
+    
+    console.log(body);
+    var body = JSON.parse(body)
+    var doneRunning = body.data_ready;
+    if(doneRunning){
+      runIsDone(runID);
+    }else{
+      setTimeout(checkRun, 5000, runID);
+    }
+  });
+}
+
+function runIsDone(runID){
+
+  request({
+    uri: 'https://www.parsehub.com/api/v2/runs/'+runID+'/data',
+    method: 'GET',
+    gzip: true,
+    qs: {
+      api_key: "tr0EdoMBubaDWcHYw0C7taFd"
+    }
+  }, function (err, resp, body) {
+    console.log(body);
+  });
+
+}
